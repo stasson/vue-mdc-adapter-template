fa03ba7b7095e11137ee9a0279d94b534f11ca97
stasson <sebastien.tasson@gmail.com>
2017-12-02 15:50:04 +0100

    chore: upgrade vue-mdc-adapter to  v0.6.0

--- src/index.js
+++ src/index.js
@@ -3,23 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueMDCAdapter from 'vue-mdc-adapter'
-// import VueMDCCard from 'vue-mdc-adapter/card'
-// import VueMDCGridList from 'vue-mdc-adapter/grid-list'
-// import VueMDCToolbar from 'vue-mdc-adapter/toolbar'
-// import VueMDCDrawer from 'vue-mdc-adapter/drawer'
-// import VueMDCLayoutApp from 'vue-mdc-adapter/layout-app'
-// import VueMDCLayoutGrid from 'vue-mdc-adapter/layout-grid'
-
+import VueMDCAdapter  from 'vue-mdc-adapter'
+import './theme.scss'
 Vue.config.productionTip = false
 
 Vue.use(VueMDCAdapter)
-// Vue.use(VueMDCCard)
-// Vue.use(VueMDCGridList)
-// Vue.use(VueMDCToolbar)
-// Vue.use(VueMDCDrawer)
-// Vue.use(VueMDCLayoutApp)
-// Vue.use(VueMDCLayoutGrid)
+
 
 /* eslint-disable no-new */
 new Vue({
