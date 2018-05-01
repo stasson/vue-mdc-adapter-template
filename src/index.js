8cc23265fb070f8e6301bfedacce19212bec51f6
stasson <stasson@users.noreply.github.com>
2018-05-01 14:24:06 +0200

    update to 0.15.3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueMDCAdapter from 'vue-mdc-adapter'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import  "vue-mdc-adapter/dist/vue-mdc-adapter.css";
+import VueMDCAdapter from "vue-mdc-adapter";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueMDCAdapter)
+Vue.use(VueMDCAdapter);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
